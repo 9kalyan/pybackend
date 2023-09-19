ee5e5bfb6a12b0651f277b2615b16c4ebc7fd6b7
9kalyan <rakalyanram@gmail.com>
2023-09-19 20:44:47 +0530

    third commit

--- index.py
+++ index.py
@@ -1,18 +1,13 @@
-import redis
 from flask import Flask,jsonify
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-r.set("man","kalyanram")
 app=Flask(__name__)
 @app.route("/")
 def func():
     return jsonify([
-                    {"index":r.get("man")},
+                    {"index":"kalyan"},
                     {"index":"poijkl"},
                     {"index":"kalyanert"},
                     {"index":"12345"},
-                    {"index":r.get("kalyan")},
+                    {"index":"1234"},
 
                     ]) 
-if __name__=="__main__":
-    app.run(port=4500)
-print(dir(redis))
+app.run(port=4500)
