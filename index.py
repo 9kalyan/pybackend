c1caf209247753776b5368ad8b905021d6a63680
9kalyan <rakalyanram@gmail.com>
2023-09-20 15:56:44 +0530

    new commit

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ def func():
                     {"index":"poijkl"},
                     {"index":"kalyanert"},
                     {"index":"12345"},
-                    {"index":"1234"},
+                    {"index":"kalyan ram is greet"},
 
                     ]) 
 app.run(port=4500,host="0.0.0.0")
