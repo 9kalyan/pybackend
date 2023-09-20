1b58a979d919da6956a33984881c75078e530766
9kalyan <rakalyanram@gmail.com>
2023-09-20 17:08:22 +0530

    new commit

--- index.py
+++ index.py
@@ -1,15 +1,11 @@
 from flask import Flask,jsonify
+import redis as rd
+r = rd.Redis(host='redis://red-ck5dggeru70s73buhagg', port=6379, decode_responses=True)
+r.set('index', 'bar')
 with open('ram.json') as kal:
     read=kal.read()
 app=Flask(__name__)
 @app.route("/")
 def func():
-    return jsonify([
-                    {"index":"kalyanram"},
-                    {"index":"poijkl"},
-                    {"index":"kalyanert"},
-                    {"index":"12345"},
-                    {"index":"kalyan ram is excellent"},
-
-                    ]) 
+    return jsonify([{"index":r.get('index')}]) 
 app.run(port=4500,host="0.0.0.0")
