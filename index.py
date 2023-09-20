9725fa8a09918824f7dff4761b0063811b465550
9kalyan <rakalyanram@gmail.com>
2023-09-20 20:38:51 +0530

    new commit

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 import redis as rd
 r = rd.Redis(host='red-ck5dggeru70s73buhagg', port=6379, decode_responses=True)
 r.set('index', 'bar')
@@ -9,7 +9,7 @@ app=Flask(__name__)
 @app.route("/",methods=['POST','GET'])
 def func():
     if request.method=='POST':
-        rd=request.get_json()
-        kal=rd['index']
-    return jsonify([{"index":kal}]) 
+        title = request.json['title']
+        return title
+    return jsonify([{"index":title}]) 
 app.run(port=4500,host="0.0.0.0")
